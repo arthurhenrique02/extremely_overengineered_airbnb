4d7f91d4a074a8edcc1288793ea3f22162568c0d
arthur.henrique02 <profissionalarthurhenrique@gmail.com>
2026-01-19 00:48:45 -0300

    chore: added endpoints for test

--- auth_service/main.py
+++ auth_service/main.py
@@ -3,6 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/health")
+@app.get("/health/live")
 async def health_check():
     return {"status": "ok"}
+
+
+@app.get("/health/ready")
+async def readiness_check():
+    return {"status": "ready"}
+
+
+@app.get("/health/startup")
+async def startup_check():
+    return {"status": "startup"}
